932bba6bf203b642efd3c2dc45e291cfcd697e13
CodeNull <code.null@yandex.ru>
2022-05-16 23:11:54 +1000

    Отформатировал код с помощью prettier.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'Angular-learn-js-180422';
+	title = 'Angular-learn-js-180422';
 }
