6add10750964599d3d563867fb71cbedc987e509
CodeNull <code.null@yandex.ru>
2022-05-30 01:22:23 +1000

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Angular-learn-js-180422';
 	property = 'property';
+
+	shopIcon = 'https://png.pngtree.com/png-clipart/20190516/original/pngtree-vector-shop-icon-png-image_3762863.jpg';
+
+	log(event: Event) {
+		event.stopPropagation();
+		console.log(event);
+	}
 }
